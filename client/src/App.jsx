2ddad7aa005db11d4ded485ceca7a0efe2e5142b
Jasser Kardamine <jasserhav@gmail.com>
2026-02-04 22:36:32 +0100

    change of plan

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -52,8 +52,14 @@ export default function App() {
         element={!user ? <UserSignUp /> : <Navigate to="/solo" replace />}
       />
 
-      <Route path="/solo" element={<ProblemsListPage />} />
-      <Route path="/problem/:slug" element={<CodeEditorPage />} />
+      <Route
+        path="/solo"
+        element={!user ? <UserLogin /> : <ProblemsListPage />}
+      />
+      <Route
+        path="/problem/:slug"
+        element={!user ? <UserLogin /> : <CodeEditorPage />}
+      />
 
       {/* you can add /signup here too */}
       <Route path="*" element={<PageNotFound />} />
